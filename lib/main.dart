15ff19a4808751a403e1b627b832680ad86c8c8b
Marco Leonardini <marco_leonardini@hotmail.com>
2020-05-03 16:12:27 -0400

    added page view and flip cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'dart:core';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
@@ -19,9 +24,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController = PageController(viewportFraction: 0.75);
+  double height, width;
+  double currentPage = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        currentPage = _pageController.page;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // print('---------------------------');
+
+    height = MediaQuery.of(context).size.height * 0.65;
+    width = MediaQuery.of(context).size.width * 0.85;
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Transform(
+                transform: (Matrix4.identity()..setEntry(3, 2, 0.002))
+                  ..rotateY((currentPage - index).abs() * (pi / 2)),
+                alignment: Alignment.center,
+                child: Center(
+                  child: Container(
+                    width: width,
+                    height: height,
+                    // constraints: BoxConstraints.loose(Size(width, height)),
+                    color: Colors.primaries[index % 17],
+                    child: Text(
+                      currentPage.toString(),
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
